a0f04e327a3c04bca6fed1e743ec7363bc890a7d
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-25 18:13:53 +0530

    Create 08-createObj.js

---
+++ 08-createObj.js
@@ -0,0 +1,12 @@
+//https://onecompiler.com/javascript/4427v4yyf
+
+const student = {
+  name : "Medha",
+  age : 24,
+  grades : [85, 90, 78, 92]
+};
+
+console.log(student.name);
+console.log(student["name"]);
+student.school = "Silver Oaks School";
+console.log(student);
